38dd12d302cde0aefb330c787849fbbbde5ce662
charlescleyton <you@example.com>
2021-02-10 16:54:35 -0300

    Inserção do Projeto apimobfin

---
+++ apimobfin/src/main/java/com/tiraiz/apimobfin/models/Usuario.java
@@ -0,0 +1,97 @@
+package com.tiraiz.apimobfin.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String pass;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String estate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEstate() {
+        return estate;
+    }
+
+    public void setEstate(String estate) {
+        this.estate = estate;
+    }
+
+}
---
+++ apimobfin/src/test/java/com/tiraiz/apimobfin/ApimobfinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiraiz.apimobfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimobfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
